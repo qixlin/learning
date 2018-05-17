2bc281e0d513f542ee423c47b03314b3e09bb643
qixlin <qixlin@hotmail.com>
2018-05-17 09:47:47 +0800

    初始化

---
+++ src/main/java/io/redos/App.java
@@ -0,0 +1,13 @@
+package io.redos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
